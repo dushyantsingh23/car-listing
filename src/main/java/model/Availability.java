635ca39b09507568f74ddaa2f07c7b7db98be8a0
Dushyant <dushyant.iiit@gmail.com>
2017-08-26 00:43:47 +0530

    Removed unused dependencies

---
+++ src/main/java/model/Availability.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dushyant
+ * Date: 25/8/17
+ * Time: 11:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Availability {
+}
